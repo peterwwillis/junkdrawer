e863084fb8ac4bea2d212f3d69aa1f959890c0f1
Peter W <31324861+peterwwillis@users.noreply.github.com>
2023-03-02 10:56:23 -0500

    Script to grab pod logs from failing pods

---
+++ k8s-get-pod-logs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+#set -eu
+[ "${DEBUG:-0}" = "1" ] && set -x
+
+mkdir -p pods
+
+kubectl get pods | while read -r line ; do
+    pod_name="$(echo "$line" | awk '{print $1}')"
+    echo "Found pod '$pod_name'"
+    if echo "$line" | grep -E 'CrashLoopBackOff|Error|Failed' ; then
+        echo "  Pod has errors; dumping logs"
+        kubectl logs "$pod_name" > pods/"$pod_name".log &
+    fi
+done
