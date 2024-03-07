dc3c4b0fad3af120817090ff5592eefd764575b1
Peter W <31324861+peterwwillis@users.noreply.github.com>
2024-03-07 12:27:37 -0500

    Properly handle worktrees and current branch

--- git-cleanup-local-stale.sh
+++ git-cleanup-local-stale.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
+[ "${DEBUG:-0}" = "1" ] && set -x
 git remote prune origin
-git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D
+git branch -vv | \grep -E '\[origin/[^:]+: gone\]' | sed -e 's/^.//' | awk '{print $1}' | xargs git branch -D
