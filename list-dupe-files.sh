afbb2f1b34eec5c20aa59a93b27bbb816701c89f
Peter W <31324861+peterwwillis@users.noreply.github.com>
2025-03-13 11:39:43 -0400

    Add new files

--- list-dupe-files.sh
+++ list-dupe-files.sh
@@ -4,4 +4,9 @@
 # Pass this script one or more files or directories, and it will print out
 # the files that are duplicates. Uses md5sum for comparison.
 
+if [ $# -lt 1 ] ; then
+    echo "Usage: $0 FILE|DIR [..]"
+    exit 1
+fi
+
 find "$@" -type f -exec md5sum {} \; | sort | awk '$1 in a{if(a[$1])print a[$1];a[$1]=""; print; next} {a[$1]=$0}'
