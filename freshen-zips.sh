3f1c677f45a797f30805cc78924c6fdb5adf97d2
Peter W <31324861+peterwwillis@users.noreply.github.com>
2026-02-22 22:31:19 -0500

    Update script

--- freshen-zips.sh
+++ freshen-zips.sh
@@ -7,10 +7,19 @@
 #
 # This is effectively a really simple backup program.
 #
-set -eu
+set -u
 [ "${DEBUG:-0}" = "1" ] && set -x
 
 for f in "$@" ; do
     [ -f "$f" ] || continue
     zip -9 -f "$f"
+    ret=$?
+    # return status 12: zip has nothing to do
+    if [ $ret -eq 0 ] || [ $ret -eq 12 ] ; then
+        continue
+    else
+        exit $ret
+    fi
 done
+
+exit 0
